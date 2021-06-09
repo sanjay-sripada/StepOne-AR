7bb0a92411cb02b319fdb96fc0b34c1874655647
sanjaysripada <sanjay.s@immersionslabs.com>
2021-06-09 11:22:13 +0530

    node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
